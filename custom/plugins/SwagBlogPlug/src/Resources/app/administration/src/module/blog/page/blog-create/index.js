df962594ef0a793675b8ce7c6ae5fd87c375ee24
Charmi Mistry <charmi@icreativetechnologies.com>
2026-02-18 15:32:42 +0530

    worked on multi select for product

--- custom/plugins/SwagBlogPlug/src/Resources/app/administration/src/module/blog/page/blog-create/index.js
+++ custom/plugins/SwagBlogPlug/src/Resources/app/administration/src/module/blog/page/blog-create/index.js
@@ -1,7 +1,7 @@
 import template from './blog-create.html.twig';
 
 const { Component, Mixin } = Shopware;
-const { Criteria } = Shopware.Data;
+const { Criteria, EntityCollection } = Shopware.Data;
 
 Component.register('blog-create', {
     template,
@@ -16,12 +16,32 @@ Component.register('blog-create', {
         return {
             blog: null,
             repository: null,
-            isLoading: false
+            isLoading: false,
+            products: null
         };
+    },
+
+    computed: {
+        productRepository() {
+            return this.repositoryFactory.create('product');
+        },
+        productCriteria() {
+            const criteria = new Criteria(1, 25);
+            criteria.addAssociation('options.group');
+            return criteria;
+        },
+        productContext() {
+            return { ...Shopware.Context.api, inheritance: true };
+        }
     },
 
     created() {
         this.repository = this.repositoryFactory.create('swag_blog');
+        this.products = new EntityCollection(
+            this.productRepository.route,
+            this.productRepository.entityName,
+            this.productContext
+        );
 
         if (this.$route.params.id) {
             this.loadEntity();
@@ -35,12 +55,34 @@ Component.register('blog-create', {
         loadEntity() {
             this.isLoading = true;
 
+        const criteria = new Criteria();
+            criteria.addAssociation('products.options.group');
+            criteria.addAssociation('products.translations');
             this.repository
-                .get(this.$route.params.id, Shopware.Context.api)
+                .get(this.$route.params.id, Shopware.Context.api, criteria)
                 .then((entity) => {
                     this.blog = entity;
+                    this.products = entity.products || new EntityCollection(
+                        this.productRepository.route,
+                        this.productRepository.entityName,
+                        this.productContext
+                    );
+                    this.blog.products = this.products;
+                })
+                .catch((error) => {
+                    console.error(error);
+                    this.createNotificationError({
+                        message: 'Error loading blog'
+                    });
+                })
+                .finally(() => {
                     this.isLoading = false;
                 });
+        },
+
+        setProductCollection(productCollection) {
+            this.products = productCollection;
+            this.blog.products = productCollection;
         },
 
         onSave() {
@@ -75,6 +117,7 @@ Component.register('blog-create', {
                 return;
             }
             this.isLoading = true;
+            this.blog.products = this.products;
 
             this.repository
                 .save(this.blog, Shopware.Context.api)
