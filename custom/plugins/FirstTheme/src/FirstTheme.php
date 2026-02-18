3a3f343b31bd3e69a4d216cc1c7b8d2df38e658b
Charmi Mistry <charmi@icreativetechnologies.com>
2026-02-18 18:38:27 +0530

    first plugin theme

---
+++ custom/plugins/FirstTheme/src/FirstTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace FirstTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class FirstTheme extends Plugin implements ThemeInterface
+{
+}
